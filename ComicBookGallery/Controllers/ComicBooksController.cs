0f6004bcb410f0cb88cc7003f27bc05af0f73995
Chica502 <oxg101@aol.com>
2018-05-30 23:30:48 -0400

    Added Detail Commit Changes

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -7,7 +7,10 @@ using System.Web.Mvc;
 namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
-
-    {
+ {
+        public string Detail()
+        {
+            return "Hello, from the Comic Books Controller";
+        }
     }
 }
